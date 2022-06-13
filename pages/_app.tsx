d201bd3b0439fed862b9991f211f41a39acf4d4a
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-06-13 10:10:41 +0200

    CacheProvider, ThemeProvider was added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
+import PropTypes from "prop-types";
+import "../styles/globals.css";
+import lightTheme from "../styles/theme/lightTheme";
+import createEmotionCache from "../utility/createEmotionCache";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface ToDoAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+function ToDoApp(props: ToDoAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
+
+export default ToDoApp;
+
+ToDoApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
