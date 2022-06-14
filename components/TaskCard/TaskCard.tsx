50f6d2b00679bede9dd399edbce0fa051384fa4e
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-06-14 18:18:30 +0200

    Props was added

--- components/TaskCard/TaskCard.tsx
+++ components/TaskCard/TaskCard.tsx
@@ -6,14 +6,18 @@ import {
   Typography,
 } from "@mui/material";
 import * as React from "react";
+import { Task } from "utility/interfaces/task";
 import CardActionsSelect from "./CardActionsSelect";
 
-const TaskCard = () => (
-  /*  */
+type TasksCardProps = {
+  task: Task;
+};
+
+const TaskCard: React.FC<TasksCardProps> = ({ task }) => (
   <Card sx={{ minWidth: 300 }}>
-    <CardHeader title="Task name" subheader="Created 14.06.2022" />
+    <CardHeader title={task?.taskName} subheader={task?.date} />
     <CardContent>
-      <Typography variant="body2">Task description</Typography>
+      <Typography variant="body2">{task?.taskDescription}</Typography>
     </CardContent>
     <CardActions>
       <CardActionsSelect />
