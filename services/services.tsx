79fbf12a8066b73cb83e0eb8491eedfef731bb95
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-06-15 23:27:19 +0200

    Unused functions was deleted

--- services/services.tsx
+++ services/services.tsx
@@ -1,9 +1,8 @@
 import axios from "axios";
-import { Task } from "type";
 
-const serverURL = process.env.NEXT_PUBLIC_SERVER_URL_MOCKEND as string;
+/* const serverURL = process.env.NEXT_PUBLIC_SERVER_URL_MOCKEND as string; */
 const authUserURL = process.env.NEXT_PUBLIC_AUTH_USER_URL_LOCAL as string;
-/* const serverURL = process.env.NEXT_PUBLIC_SERVER_URL_LOCAL as string; */
+const serverURL = process.env.NEXT_PUBLIC_SERVER_URL_LOCAL as string;
 
 const getTasks = async () => {
   const response = await axios.get(serverURL);
@@ -17,28 +16,4 @@ const getUser = async () => {
   return response.data;
 };
 
-const createNewTask = async (newTask: Task) => {
-  await axios({
-    url: serverURL,
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    data: { ...newTask, state: "ToDo" },
-  });
-};
-
-const deleteTask = async (id: string) => {
-  await axios({
-    url: `${serverURL}/${id}`,
-    method: "DELETE",
-  });
-};
-
-const updateTask = async (task: Task) => {
-  await axios({
-    url: `${serverURL}/${task.id}`,
-    method: "PUT",
-    data: task,
-  });
-};
-
-export { getTasks, createNewTask, deleteTask, updateTask, getUser };
+export { getTasks, getUser };
