51f40b828fead6ff9db78ec042978d63e6971c98
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-06-15 18:24:40 +0200

    TaskReducer created

---
+++ redux/reducers/taskReducer.tsx
@@ -0,0 +1,33 @@
+import { IAction, Task } from "type";
+import actionTypes from "../actions/actionTypes";
+
+const taskReducer = (action: IAction, tasks = []) => {
+  let newTask;
+
+  switch (action.type) {
+    case actionTypes.loadTasks:
+      newTask = [...(action.tasks as Task[])];
+      break;
+
+    case actionTypes.deleteTask:
+      newTask = tasks.filter((task: Task) => task.id !== action.id);
+      break;
+
+    case actionTypes.createTask:
+      newTask = [...tasks, action.task];
+      break;
+
+    case actionTypes.updateTask:
+      newTask = tasks.map((task: Task) =>
+        task.id === action.task.id ? { ...task, ...action.task } : task
+      );
+      break;
+
+    default:
+      newTask = [...tasks];
+  }
+
+  return newTask;
+};
+
+export default taskReducer;
