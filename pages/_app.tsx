22993ed33640019a396857a055e443a487a0384b
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-06-15 19:08:52 +0200

    Redux provider added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,28 +5,34 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 import { SnackbarProvider } from "notistack";
 import PropTypes from "prop-types";
+import { Provider } from "react-redux";
 import "../styles/globals.css";
+import configureStore from "redux/store";
 import createEmotionCache from "../utility/createEmotionCache";
 
 interface ToDoAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
+const store = configureStore();
+
 const clientSideEmotionCache = createEmotionCache();
 
 function ToDoApp(props: ToDoAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={lightTheme}>
-        <SnackbarProvider maxSnack={3}>
-          <CssBaseline />
-          <UserProvider>
-            <Component {...pageProps} />
-          </UserProvider>
-        </SnackbarProvider>
-      </ThemeProvider>
-    </CacheProvider>
+    <Provider store={store}>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={lightTheme}>
+          <SnackbarProvider maxSnack={3}>
+            <CssBaseline />
+            <UserProvider>
+              <Component {...pageProps} />
+            </UserProvider>
+          </SnackbarProvider>
+        </ThemeProvider>
+      </CacheProvider>
+    </Provider>
   );
 }
 
