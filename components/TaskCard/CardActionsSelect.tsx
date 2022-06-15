cb385c66ba1637ae9210a2b35b9611a612223b67
Aleksandr Turchenko <alexandrt100@gmail.com>
2022-06-15 05:38:32 +0200

    Snackbar for update task state was added

--- components/TaskCard/CardActionsSelect.tsx
+++ components/TaskCard/CardActionsSelect.tsx
@@ -10,15 +10,21 @@ import * as React from "react";
 import { updateTask } from "services/services";
 import { CardActionsSelectProps } from "type";
 import { Task } from "utility/interfaces/task";
+import { useSnackbar } from "notistack";
 
 const CardActionsSelect: React.FC<CardActionsSelectProps> = ({
   state,
   setState,
   task,
 }) => {
+  const { enqueueSnackbar } = useSnackbar();
+
   const handleChange = (event: SelectChangeEvent<typeof state>) => {
     setState(event.target.value as Task["state"]);
     updateTask({ ...task, state: event.target.value as Task["state"] });
+    enqueueSnackbar(`Task state was changed to ${event.target.value}`, {
+      variant: "success",
+    });
   };
 
   return (
@@ -35,7 +41,7 @@ const CardActionsSelect: React.FC<CardActionsSelectProps> = ({
           label="Status"
         >
           <MenuItem value={"ToDo"}>ToDo</MenuItem>
-          <MenuItem value={"InProgress"}>InProgress</MenuItem>
+          <MenuItem value={"InProgress"}>In Progress</MenuItem>
           <MenuItem value={"Done"}>Done</MenuItem>
         </Select>
       </FormControl>
